dc239a2a7b9c34510218fab13b2afdbdabe89b3a
MarinEstrada <adrian.marin.estrada@gmail.com>
2024-08-11 22:55:59 -0700

    WIP: start of Education section

---
+++ src/components/education/Education.jsx
@@ -0,0 +1,12 @@
+import DATA from '../../constant/mockData';
+import Title from '../common/Title';
+import PropTypes from 'prop-types';
+
+const Education = () => {
+    return (
+        <div className='education-sc resume-block'>
+        </div>
+    );
+}
+
+export default Education;
