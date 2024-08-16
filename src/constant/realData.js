4299fef3aaa9ab87e48fbc4bf35c07715a01716c
MarinEstrada <adrian.marin.estrada@gmail.com>
2024-08-16 11:31:58 -0700

    WIP: framework to add personalized data in future

---
+++ src/constant/realData.js
@@ -0,0 +1,46 @@
+import {
+  Blogging,
+  CompanyLogo1,
+  CompanyLogo2,
+  CompanyLogo3,
+  CompanyLogo4,
+  Coursera,
+  DiamondLgBlue,
+  DiamondLgGreen,
+  DiamondLgOrange,
+  DiamondLgPink,
+  DiamondLgYellow,
+  Gaming,
+  Google,
+  Reading,
+  SkillCss,
+  SkillFigma,
+  SkillHtml5,
+  SkillPhotoshop,
+  SkillSketch,
+  SkillTrello,
+  SkillWireframe,
+  SkillXd,
+  Travelling,
+  Udemy,
+} from "../assets/image";
+
+const DATA = {
+  professionalExperiences: [
+
+  ],
+  educationalExperiences: [
+
+  ],
+  skills: [
+
+  ],
+  certificatesAndAwards: [
+
+  ],
+  hobbiesAndInterests: [
+
+  ],
+};
+
+export default DATA;
