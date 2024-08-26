c181ace6838d117371acdedf57f9b52d7dadab66
MarinEstrada <adrian.marin.estrada@gmail.com>
2024-08-26 14:45:59 -0700

    WIP: set base to github pages url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "./cv-site/",
-  base: "./cv-site/dist",
+  base: "https://marinestrada.github.io/cv-site/",
 })
