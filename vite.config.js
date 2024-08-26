4e38e9815b7533ab068fab02e7626c7e86d9d611
MarinEstrada <adrian.marin.estrada@gmail.com>
2024-08-26 15:55:24 -0700

    WIP: rever to previous actions, change base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://marinestrada.github.io/cv-site/",
+  base: "https://marinestrada.github.io/cv-site/dist/",
 })
