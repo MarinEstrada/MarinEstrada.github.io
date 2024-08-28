9a696ba7c0981e7574497e91a82ff0b156d33ace
MarinEstrada <adrian.marin.estrada@gmail.com>
2024-08-28 16:37:56 -0700

    DOCS: change base for new url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://marinestrada.github.io/cv-site/",
+  base: "/",
+  // base: "https://marinestrada.github.io/cv-site/",
   // base: "/cv-site/",
 })
